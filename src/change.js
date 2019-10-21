daf9d9ffa66caba6521d219b0c9831884da3714f
Ryan Wilson <rwilson@ucvts.org>
2019-10-21 10:01:30 -0400

    updated starter code for exercise 1

--- src/change.js
+++ src/change.js
@@ -6,12 +6,9 @@ const maximum = 0.99;
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    console.log("NaN");
-// print your error message here
+    // print your error message here
 } else if (amount < minimum || amount > maximum) {
-    console.log("Out of range");
-// print your error message here
+    // print your error message here
 } else {
-    console.log("Make change");
-// write your code for making change here
+    // write your code for making change here
 }
