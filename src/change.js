1c209cf9081f61448e9b3bcc5b952dbf265c369f
divanov2004 <55397915+divanov2004@users.noreply.github.com>
2019-10-30 10:31:46 -0400

    bob the builder

--- src/change.js
+++ src/change.js
@@ -5,10 +5,25 @@ const MAX = 0.99;
 
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
+const quarterValue = 0.25;
+const dimeValue = 0.10;
+const nickelValue = 0.05;
+const pennyValue = 0.01;
+
+
+
+
+
+
+
+
+
+
+
 if (Number.isNaN(amount)) {
     // print your error message here
 } else if (amount < MIN || amount > MAX) {
     // print your error message here
 } else {
-    // write your code for making change here
+console.log("");
 }
